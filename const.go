2dcf487d1b4fa3ce89a4e8df452ea1ffec28aba3
pinke <pinke.yu7@gmail.com>
2022-06-23 10:37:22 +0800

    Feature: Yating TTS SDK

---
+++ const.go
@@ -0,0 +1,19 @@
+package YatingTtsSdk
+
+const (
+	ModelFemale1 = "zh_en_female_1"
+	ModelFemale2 = "zh_en_female_2"
+	ModelMale1   = "zh_en_male_1"
+
+	TypeText = "text"
+
+	EncodingMp3      = "MP3"
+	EncodingLinear16 = "LINEAR16"
+
+	SampleRate16k = "16K"
+)
+
+var Model = []string{ModelFemale1, ModelFemale2, ModelMale1}
+var Type = []string{TypeText}
+var Encoding = []string{EncodingMp3, EncodingLinear16}
+var SampleRate = []string{SampleRate16k}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TaiwanAILabs-Yating/tts-sdk-go
+
+go 1.17
