4e618573292dad076aeafb46fcd866e4805765fd
pinke <pinke.yu7@gmail.com>
2022-08-30 10:15:28 +0800

    Feature: type ssml

--- const.go
+++ const.go
@@ -6,6 +6,7 @@ const (
 	ModelMale1   = "zh_en_male_1"
 
 	TypeText = "text"
+	TypeSsml = "ssml"
 
 	EncodingMp3      = "MP3"
 	EncodingLinear16 = "LINEAR16"
@@ -14,6 +15,6 @@ const (
 )
 
 var Model = []string{ModelFemale1, ModelFemale2, ModelMale1}
-var Type = []string{TypeText}
+var Type = []string{TypeText, TypeSsml}
 var Encoding = []string{EncodingMp3, EncodingLinear16}
 var SampleRate = []string{SampleRate16k}
