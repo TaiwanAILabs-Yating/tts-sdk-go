6717575cf2b3c7ea3d9b3fcfad831375691d6e6d
pinke <pinke.yu7@gmail.com>
2023-10-20 11:50:23 +0800

    Feature: upgrade to tts v2 api

--- const.go
+++ const.go
@@ -1,9 +1,13 @@
 package YatingTtsSdk
 
 const (
-	ModelFemale1 = "zh_en_female_1"
-	ModelFemale2 = "zh_en_female_2"
-	ModelMale1   = "zh_en_male_1"
+	ModelZhEnFemale1 = "zh_en_female_1"
+	ModelZhEnFemale2 = "zh_en_female_2"
+	ModelZhEnMale1   = "zh_en_male_1"
+
+	ModelTaiFemale1 = "tai_female_1"
+	ModelTaiFemale2 = "tai_female_2"
+	ModelTaiMale1   = "tai_male_1"
 
 	TypeText = "text"
 	TypeSsml = "ssml"
@@ -12,9 +16,17 @@ const (
 	EncodingLinear16 = "LINEAR16"
 
 	SampleRate16k = "16K"
+	SampleRate22k = "22K"
 )
 
-var Model = []string{ModelFemale1, ModelFemale2, ModelMale1}
+var Model = []string{ModelZhEnFemale1, ModelZhEnFemale2, ModelZhEnMale1, ModelTaiFemale1, ModelTaiFemale2, ModelTaiMale1}
 var Type = []string{TypeText, TypeSsml}
 var Encoding = []string{EncodingMp3, EncodingLinear16}
-var SampleRate = []string{SampleRate16k}
+var SampleRateMapping = map[string][]string{
+	ModelZhEnFemale1: {SampleRate16k, SampleRate22k},
+	ModelZhEnFemale2: {SampleRate16k, SampleRate22k},
+	ModelZhEnMale1:   {SampleRate16k, SampleRate22k},
+	ModelTaiFemale1:  {SampleRate16k},
+	ModelTaiFemale2:  {SampleRate16k},
+	ModelTaiMale1:    {SampleRate16k},
+}
